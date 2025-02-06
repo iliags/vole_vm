3476977e38dfb3a8d064dd4e252c51794fe25d5a
iliags <19849002+iliags@users.noreply.github.com>
2025-02-06 01:49:56 -0700

    Update app.rs

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -1,8 +1,12 @@
 use super::{rom::Rom, NumericDisplay, SourceEditMode};
 use crate::vole::{StartMode, Vole};
 use egui::{
-    containers::Frame, emath, epaint, epaint::PathStroke, hex_color, lerp, pos2, remap,
-    scroll_area::ScrollBarVisibility, vec2, Color32, Pos2, Rect,
+    containers::Frame,
+    emath,
+    epaint::{self, PathStroke},
+    hex_color, lerp, pos2, remap,
+    scroll_area::ScrollBarVisibility,
+    vec2, Color32, Pos2, Rect, Scene, Vec2,
 };
 use egui_code_editor::{CodeEditor, ColorTheme, Syntax};
 use regex::Regex;
@@ -97,6 +101,9 @@ pub struct VoleUI {
 
     #[serde(skip)]
     cycle_speed: usize,
+
+    #[serde(skip)]
+    scene_rect: Rect,
 }
 
 impl Default for VoleUI {
@@ -121,6 +128,7 @@ impl Default for VoleUI {
             show_export: false,
             cycle_timer: 0.0,
             cycle_speed: 0,
+            scene_rect: Rect::ZERO,
         }
     }
 }
@@ -195,6 +203,11 @@ impl eframe::App for VoleUI {
            Source code editing panel
         */
         egui::SidePanel::left("Source Panel").show(ctx, |ui| {
+            egui::ScrollArea::vertical()
+                //.max_height(400.0)
+                //.auto_shrink(false)
+                .scroll_bar_visibility(ScrollBarVisibility::AlwaysVisible)
+                .show(ui, |ui| {});
             ui.heading("Program Source Code");
             // Source edit mode selection
             egui::ComboBox::from_label("Edit mode")
@@ -521,63 +534,7 @@ impl eframe::App for VoleUI {
            Visualizer panel
         */
         egui::CentralPanel::default().show(ctx, |ui| {
-            Frame::canvas(ui.style()).show(ui, |ui| {
-                let color = if ui.visuals().dark_mode {
-                    Color32::from_additive_luminance(196)
-                } else {
-                    Color32::from_black_alpha(240)
-                };
-
-                ui.ctx().request_repaint();
-                let time = ui.input(|i| i.time);
-
-                let desired_size = ui.available_width() * vec2(1.0, 0.5);
-                let (_id, rect) = ui.allocate_space(desired_size);
-
-                let to_screen = emath::RectTransform::from_to(
-                    Rect::from_x_y_ranges(0.0..=1.0, -1.0..=1.0),
-                    rect,
-                );
-
-                let mut shapes = vec![];
-
-                for &mode in &[2, 3, 5] {
-                    let mode = mode as f64;
-                    let n = 120;
-                    let speed = 1.5;
-
-                    let points: Vec<Pos2> = (0..=n)
-                        .map(|i| {
-                            let t = i as f64 / (n as f64);
-                            let amp = (time * speed * mode).sin() / mode;
-                            let y = amp * (t * std::f64::consts::TAU / 2.0 * mode).sin();
-                            to_screen * pos2(t as f32, y as f32)
-                        })
-                        .collect();
-
-                    let thickness = 10.0 / mode as f32;
-                    shapes.push(epaint::Shape::line(
-                        points,
-                        if true {
-                            PathStroke::new_uv(thickness, move |rect, p| {
-                                let t = remap(p.x, rect.x_range(), -1.0..=1.0).abs();
-                                let center_color = hex_color!("#5BCEFA");
-                                let outer_color = hex_color!("#F5A9B8");
-
-                                Color32::from_rgb(
-                                    lerp(center_color.r() as f32..=outer_color.r() as f32, t) as u8,
-                                    lerp(center_color.g() as f32..=outer_color.g() as f32, t) as u8,
-                                    lerp(center_color.b() as f32..=outer_color.b() as f32, t) as u8,
-                                )
-                            })
-                        } else {
-                            PathStroke::new(thickness, color)
-                        },
-                    ));
-                }
-
-                ui.painter().extend(shapes);
-            });
+            ui.label("Under Construction");
         });
     }
 }
