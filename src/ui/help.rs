a92f6c2a517fcdb3c3bed6c7675190b63328e79c
iliags <19849002+iliags@users.noreply.github.com>
2025-02-11 04:11:53 -0700

    Update help.rs

--- src/ui/help.rs
+++ src/ui/help.rs
@@ -1,4 +1,6 @@
-pub const ASM_SYNTAX: &str = r"ld dest,src - Load a value from src into dest
+pub const ASM_SYNTAX: &str = r".org value - Offset the program by value, emulator specific instruction.
+
+ld dest,src - Load a value from src into dest. Src can be a memory address, register, or value. Dest can be a memory address or register.
 
 adds r,s,t - Adds registers s and t as two's compliment, stores result in register r.
 
@@ -14,4 +16,8 @@ rot r,x - Rotates the bit pattern in register r to the right x times.
 
 jp r,<label> - Jump to the label if register r is equal to register 0
 
-halt - Stop program execution";
+halt - Stop program execution
+
+r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, ra, rb, rc, rd, re, rf - Registers.
+
+Memory addresses are a hex or binary value surrounded by parentheses, values are hex (prefix 0x) or binary (prefix 0b) patterns.";
